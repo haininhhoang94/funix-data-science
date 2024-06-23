cb52de20fcc7520ceab6264da11e13e020469d73
Hai Ninh Hoang <haininhhoang94@gmail.com>
2024-06-23 23:21:26 +0700

    updates from macbook for sync

--- DSP303x/supplement/decision-tree-id3.py
+++ DSP303x/supplement/decision-tree-id3.py
@@ -9,4 +9,15 @@ class TreeNode(object):
         self.ids = ids  # index of data in this node
         self.entropy = entropy  # entropy
         self.depth = depth  # distance to root node
-        self.split_attribute = None
+        self.split_attribute = (
+            None  # which attributre is chosen, it is a nonleaf node
+        )
+        self.children = children  # lists of its child nodes
+        self.order = None  # order of values of split_attribute in children
+        self.label = None  # label of node if it is a leaf
+
+    def set_properties(self, split_attribute, order):
+        self.split_attribute = split_attribute  # split at which attribute
+        self.order = order  # order of this node's children
+
+    def set_label(self, label):
